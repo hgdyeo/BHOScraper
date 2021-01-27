c9c8f9f939df0abb07b72ff4f5554228b9f38da8
Henry Yeomans <henryyeomans@kubrickgroup.com>
2021-01-27 16:04:17 +0000

    Removes commented-out block in cli.py

--- src/bho_scraper/cli.py
+++ src/bho_scraper/cli.py
@@ -20,18 +20,7 @@ def scrape(series, queries, path):
     queries = [str(item) for item in queries.strip('[]').split(',')]
     scraper = BHOScraper()
     scraper.scrape_series(series, queries, path)
-    # scraper = BHOScraper()
-    # if type(series) == str:
-    #     series = [series]
-    # if type(queries) == str:
-    #     queries = [queries]
-  
-    # with click.progressbar(series) as series_bar:
-    #     for series_name in series_bar:
-    #         click.echo("Scraping {}".format(series_name))
-    #         with click.progressbar(queries) as queries_bar:
-    #             for query in queries_bar:
-    #                 scraper.scrape_series(series_queries=series_name, queries=query, path=path)
+
     click.echo("==================== SCRAPING COMPLETED ====================")
     click.echo("*.csv files saved to: {}".format(path))
     click.echo("============================================================")
